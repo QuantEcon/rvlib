1864cf7d1fd77e21a9bf8b3a50db6a816f255a0f
Isaiah Norton <isaiah.norton@gmail.com>
2014-03-21 23:55:10 -0400

    Update signature for randmtzig_gv_randn

--- src/librandom.c
+++ src/librandom.c
@@ -1,7 +1,7 @@
 /* Use librandom from the julia source tree */
 
 extern double dsfmt_gv_genrand_close_open();
-extern double randmtzig_randn (void);
+extern double randmtzig_gv_randn (void);
 extern double randmtzig_exprnd (void);
 
 double unif_rand(void) {
@@ -13,5 +13,5 @@ double exp_rand(void) {
 }
 
 double norm_rand(void) {
-    return randmtzig_randn();
+    return randmtzig_gv_randn();
 }
