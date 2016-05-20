f88008ad2a3c26f67fd7551fe9a356b69bb8c0fe
Spencer Lyon <spencerlyon2@gmail.com>
2016-05-20 13:20:03 -0700

    Added example of generating functions for norm

--- py/build_rmath.py
+++ py/build_rmath.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import textwrap
 # import platform
 
 from cffi import FFI
@@ -29,17 +30,125 @@ ffi.cdef('''\
 // Normal Distribution
 double dnorm(double, double, double, int);
 double pnorm(double, double, double, int, int);
-
-// Uniform Distribution
-double dunif(double, double, double, int);
-double punif(double, double, double, int, int);
-
-// Gamma Distribution
-double dgamma(double, double, double, int);
-double pgamma(double, double, double, int, int);
+double rnorm(double, double);
+double qnorm(double, double, double, int, int);
 ''')
 
+
+def _import_rmath(rname, pyname, *pargs):
+    """
+    # now we map from the _rmath_ffi.lib.Xnorm functions
+    # to the friendly names from the julia file here:
+    # https://github.com/JuliaStats/StatsFuns.jl/blob/master/src/rmath.jl
+    """
+    # extract Rmath.h function names
+    dfun = "d{}".format(rname)
+    pfun = "p{}".format(rname)
+    qfun = "q{}".format(rname)
+    rfun = "r{}".format(rname)
+
+    # construct python function names
+    pdf = "{}_pdf".format(pyname)
+    cdf = "{}_cdf".format(pyname)
+    ccdf = "{}_ccdf".format(pyname)
+
+    logpdf = "{}_logpdf".format(pyname)
+    logcdf = "{}_logcdf".format(pyname)
+    logccdf = "{}_loccdf".format(pyname)
+
+    invcdf = "{}_invcdf".format(pyname)
+    invccdf = "{}_invccdf".format(pyname)
+    invlogcdf = "{}_invlogcdf".format(pyname)
+    invlogccdf = "{}_invlogccdf".format(pyname)
+
+    rand = "{}_rand".format(pyname)
+
+    # make sure all names are available
+    has_rand = True
+    if rname == "nbeta" or rname == "nf" or rname == "nt":
+        has_rand = False
+
+    p_args = ", ".join(pargs)
+
+    code = """\
+    import _rmath_ffi
+    from numba import vectorize, jit
+    from numba import cffi_support
+
+    cffi_support.register_module(_rmath_ffi)
+
+    {dfun} = _rmath_ffi.lib.{dfun}
+    {pfun} = _rmath_ffi.lib.{pfun}
+    {qfun} = _rmath_ffi.lib.{qfun}
+
+    @vectorize(nopython=True)
+    def {pdf}({p_args}, x):
+        return {dfun}(x, {p_args}, 0)
+
+
+    @vectorize(nopython=True)
+    def {logpdf}({p_args}, x):
+        return {dfun}(x, {p_args}, 1)
+
+
+    @vectorize(nopython=True)
+    def {cdf}({p_args}, x):
+        return {pfun}(x, {p_args}, 1, 0)
+
+
+    @vectorize(nopython=True)
+    def {ccdf}({p_args}, x):
+        return {pfun}(x, {p_args}, 0, 0)
+
+
+    @vectorize(nopython=True)
+    def {logcdf}({p_args}, x):
+        return {pfun}(x, {p_args}, 1, 1)
+
+
+    @vectorize(nopython=True)
+    def {logccdf}({p_args}, x):
+        return {pfun}(x, {p_args}, 0, 1)
+
+
+    @vectorize(nopython=True)
+    def {invcdf}({p_args}, q):
+        return {qfun}(q, {p_args}, 1, 0)
+
+
+    @vectorize(nopython=True)
+    def {invccdf}({p_args}, q):
+        return {qfun}(q, {p_args}, 0, 0)
+
+
+    @vectorize(nopython=True)
+    def {invlogcdf}({p_args}, lq):
+        return {qfun}(lq, {p_args}, 1, 1)
+
+
+    @vectorize(nopython=True)
+    def {invlogccdf}({p_args}, lq):
+        return {qfun}(lq, {p_args}, 0, 1)
+
+    """.format(**locals())
+
+    # write code to file
+    with open("rmath_{}.py".format(pyname), "w") as f:
+        f.write(textwrap.dedent(code))
+
+    if has_rand:
+        rand_code = """\
+        {rfun} = _rmath_ffi.lib.{rfun}
+
+        @jit(nopython=True)
+        def {rand}({p_args}):
+            return {rfun}({p_args})
+
+        """.format(**locals())
+        with open("rmath_{}.py".format(pyname), "a") as f:
+            f.write(textwrap.dedent(rand_code))
+
+
 if __name__ == '__main__':
-    # Normally set verbose to `True`, but silence output
-    # for reduced notebook noise
-    ffi.compile(verbose=True)
+    # ffi.compile(verbose=False)
+    _import_rmath("norm", "norm", "mu", "sigma")
