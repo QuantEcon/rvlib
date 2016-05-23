a285b725b1562bcecb25adafb31b0a4070cbef2b
Spencer Lyon <spencerlyon2@gmail.com>
2016-05-23 14:41:08 -0700

    Fix up the Normal prototype

--- py/test_normal.py
+++ py/test_normal.py
@@ -25,198 +25,183 @@ import numpy as np
 from numba import vectorize, jit
 from math import inf
 
-
 #  ------
 #  Normal
 #  ------
 
-
 class Normal():
-	"""
-	The Normal distribution with mean mu and standard deviation sigma.
+    """
+    The Normal distribution with mean mu and standard deviation sigma.
 
-	Parameters
-	----------
-	mu : scalar(float)
-		Mean of the normal distribution
-	sigma : scalar(float)
-		Standard deviaton of the normal distribution
-	"""
+    Parameters
+    ----------
+    mu : scalar(float)
+        Mean of the normal distribution
+    sigma : scalar(float)
+        Standard deviaton of the normal distribution
+    """
 
-	def __init__(self, mu, sigma):
-		self.mu = mu
-		self.sigma = sigma
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
 
-	# ===================
-	# Parameter retrieval
-	# ===================
+    # ===================
+    # Parameter retrieval
+    # ===================
 
-	@property
-	def params(self):
-		"""Returns parameters."""
-		return (self.mu, self.sigma)
+    @property
+    def params(self):
+        """Returns parameters."""
+        return (self.mu, self.sigma)
 
-	@property
-	def location(self):
-		"""Returns parameters."""
-		return self.mu
+    @property
+    def location(self):
+        """Returns parameters."""
+        return self.mu
 
-	@property
-	def scale(self):
-		"""Returns parameters."""
-		return self.sigma
-	
-	# ==========
-	# Statistics
-	# ==========
+    @property
+    def scale(self):
+        """Returns parameters."""
+        return self.sigma
 
-	@property
-	def mean(self):
-		"""Returns mean."""
-		return self.mu
+    # ==========
+    # Statistics
+    # ==========
 
-	@property
-	def median(self):
-		"""Returns median."""
-		return self.mu
+    @property
+    def mean(self):
+        """Returns mean."""
+        return self.mu
 
-	@property
-	def mode(self):
-		"""Returns mode."""
-		return self.mu
+    @property
+    def median(self):
+        """Returns median."""
+        return self.mu
 
-	@property
-	def var(self):
-		"""Returns variance."""
-		return self.sigma ** 2
-	
-	@property
-	def std(self):
-		"""Returns standard deviation."""
-		return self.sigma
+    @property
+    def mode(self):
+        """Returns mode."""
+        return self.mu
 
-	@property
-	def skewness(self):
-		"""Returns skewness."""
-		return 0.0
+    @property
+    def var(self):
+        """Returns variance."""
+        return self.sigma ** 2
 
-	@property
-	def kurtosis(self):
-		"""Returns kurtosis."""
-		return 0.0
+    @property
+    def std(self):
+        """Returns standard deviation."""
+        return self.sigma
 
-	@property
-	def isplatykurtic(self):
-		"""Kurtosis being greater than zero."""
-		return self.kurtosis > 0
+    @property
+    def skewness(self):
+        """Returns skewness."""
+        return 0.0
 
-	@property
-	def isleptokurtic(self):
-		"""Kurtosis being smaller than zero."""
-		return self.kurtosis < 0
+    @property
+    def kurtosis(self):
+        """Returns kurtosis."""
+        return 0.0
 
-	@property
-	def ismesokurtic(self):
-		"""Kurtosis being equal to zero."""
-		return self.kurtosis == 0.0
+    @property
+    def isplatykurtic(self):
+        """Kurtosis being greater than zero."""
+        return self.kurtosis > 0
 
-	@property
-	def entropy(self):
-		"""Returns entropy."""
-		return  0.5 * (np.log(2*np.pi) + 1.0) + np.log(self.sigma)
+    @property
+    def isleptokurtic(self):
+        """Kurtosis being smaller than zero."""
+        return self.kurtosis < 0
 
-	@property
-	@vectorize(nopython=True)
-	def mgf(self, x):
-		"""Evaluate moment generating function at x."""
-		return  np.exp(x * self.mu + 0.5 * self.sigma**2 * x**2)
+    @property
+    def ismesokurtic(self):
+        """Kurtosis being equal to zero."""
+        return self.kurtosis == 0.0
 
-	@property
-	@vectorize(nopython=True)
-	def cf(self, x):
-		"""Evaluate characteristic function at x."""
-		return  np.exp(1j * x * self.mu - 0.5 * self.sigma**2 * x**2)
+    @property
+    def entropy(self):
+        """Returns entropy."""
+        return 0.5 * (np.log(2*np.pi) + 1.0) + np.log(self.sigma)
 
-	# ==========
-	# Evaluation
-	# ==========
+    @vectorize(nopython=True)
+    def mgf(self, x):
+        """Evaluate moment generating function at x."""
+        return np.exp(x * self.mu + 0.5 * self.sigma**2 * x**2)
 
-	@property
-	@vectorize(nopython=True)
-	def insupport(self, x):
-		"""When x is a scalar, it returns whether x is within the support of
-		the distribution. When x is an array, it returns whether every element"""
-		return  -inf < x < inf
+    @vectorize(nopython=True)
+    def cf(self, x):
+        """Evaluate characteristic function at x."""
+        return np.exp(1j * x * self.mu - 0.5 * self.sigma**2 * x**2)
 
-	@property
-	def pdf(self, x):
-		"""The pdf value(s) evaluated at x."""
-		return  norm_pdf(self.mu, self.sigma, x)
+    # ==========
+    # Evaluation
+    # ==========
 
+    def insupport(self, x):
+        """When x is a scalar, it returns whether x is within the support of
+        the distribution. When x is an array, it returns whether every element"""
+        return -inf < x < inf
 
-	@property
-	def logpdf(self, x):
-		"""The logarithm of the pdf value(s) evaluated at x."""
-		return  norm_logpdf(self.mu, self.sigma, x)
+    def pdf(self, x):
+        """The pdf value(s) evaluated at x."""
+        return rmath_norm.norm_pdf(self.mu, self.sigma, x)
 
-	@property
-	def loglikelihood(self, x):
-		"""The log-likelihood of the Normal distribution w.r.t. all
-		samples contained in array x."""
-		return sum(norm_logpdf(self.mu, self.sigma, x))
+    def logpdf(self, x):
+        """The logarithm of the pdf value(s) evaluated at x."""
+        return rmath_norm.norm_logpdf(self.mu, self.sigma, x)
 
-	@property
-	def cdf(self, x):
-		"""The cdf value(s) evaluated at x."""
-		return  norm_cdf(self.mu, self.sigma, x)
+    def loglikelihood(self, x):
+        """The log-likelihood of the Normal distribution w.r.t. all
+        samples contained in array x."""
+        return sum(rmath_norm.norm_logpdf(self.mu, self.sigma, x))
 
-	@property
-	def logcdf(self, x):
-		"""The logarithm of the cdf value(s) evaluated at x."""
-		return  norm_logcdf(self.mu, self.sigma, x)
+    def cdf(self, x):
+        """The cdf value(s) evaluated at x."""
+        return rmath_norm.norm_cdf(self.mu, self.sigma, x)
 
-	@property
-	def logcdf(self, x):
-		"""The logarithm of the cdf value(s) evaluated at x."""
-		return  norm_logcdf(self.mu, self.sigma, x)
+    def logcdf(self, x):
+        """The logarithm of the cdf value(s) evaluated at x."""
+        return rmath_norm.norm_logcdf(self.mu, self.sigma, x)
 
-	@property
-	def ccdf(self, x):
-		"""The complementary cdf evaluated at x, i.e. 1- cdf(x)."""
-		return  norm_ccdf(self.mu, self.sigma, x)
+    def logcdf(self, x):
+        """The logarithm of the cdf value(s) evaluated at x."""
+        return rmath_norm.norm_logcdf(self.mu, self.sigma, x)
 
-	@property
-	def logccdf(self, x):
-		"""The logarithm of the complementary cdf evaluated at x."""
-		return  norm_logccdf(self.mu, self.sigma, x)
+    def ccdf(self, x):
+        """The complementary cdf evaluated at x, i.e. 1- cdf(x)."""
+        return rmath_norm.norm_ccdf(self.mu, self.sigma, x)
 
-	@property
-	def quantile(self, q):
-		"""The quantile value evaluated at q."""
-		return  norm_invcdf(self.mu, self.sigma, q)
+    def logccdf(self, x):
+        """The logarithm of the complementary cdf evaluated at x."""
+        return rmath_norm.norm_logccdf(self.mu, self.sigma, x)
 
-	@property
-	def cquantile(self, q):
-		"""The complementary quantile value evaluated at q."""
-		return  norm_invccdf(self.mu, self.sigma, q)
+    def quantile(self, q):
+        """The quantile value evaluated at q."""
+        return rmath_norm.norm_invcdf(self.mu, self.sigma, q)
 
-	@property
-	def invlogcdf(self, lq):
-		"""The inverse function of logcdf."""
-		return  norm_invlogcdf(self.mu, self.sigma, lq)
+    def cquantile(self, q):
+        """The complementary quantile value evaluated at q."""
+        return rmath_norm.norm_invccdf(self.mu, self.sigma, q)
 
-	@property
-	def invlogccdf(self, lq):
-		"""The inverse function of logccdf."""
-		return  norm_invlogccdf(self.mu, self.sigma, lq)
+    def invlogcdf(self, lq):
+        """The inverse function of logcdf."""
+        return rmath_norm.norm_invlogcdf(self.mu, self.sigma, lq)
 
-	# ========
-	# Sampling
-	# ========
+    def invlogccdf(self, lq):
+        """The inverse function of logccdf."""
+        return rmath_norm.norm_invlogccdf(self.mu, self.sigma, lq)
 
-	@property
-	def rand(self, n=1):
-		"""Generates a random draw from the distribution."""
-		return  np.ndarray([norm_rand(self.mu, self.sigma) for x in range(n)])
+    # ========
+    # Sampling
+    # ========
 
-	
+    def rand(self, *n):
+        """Generates a random draw from the distribution."""
+        if len(n) == 0:
+            n = (1,)
+
+        out = np.empty(n)
+        for i, _ in np.ndenumerate(out):
+            out[i] = rmath_norm.norm_rand(self.mu, self.sigma)
+
+        return out
