f01144562756c27e7468da2527a9abbf692443da
danielcsaba <csaba.daniel@gmail.com>
2016-07-06 16:34:46 -0400

    setup

---
+++ py/__init__.py
@@ -0,0 +1,11 @@
+"""
+Distributions.py: mimics the interface of Distributions.jl
+<https://github.com/JuliaStats/Distributions.jl> while at the same time
+attaining similar performance exploiting numba<http://numba.pydata.org/>.
+
+@date: 07.06.2016
+
+"""
+__version__ = 0.1
+
+from .univariate import *
