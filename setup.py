3bf832fdd051b4db9cbac7903cef3e2619c50a5e
danielcsaba <csaba.daniel@gmail.com>
2016-07-06 17:06:47 -0400

    rename

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ long_desc = open(os.path.join(rootdir, 'README.md')).read()
 VERSION = "0.0.1"
 
 def write_version_py(filename=None):
-        doc = ("\"\"\"\n" +
-               "This is a VERSION file and should NOT be manually altered"
-               + "\n\"\"\"")
-        doc += "\nversion = \"%s\"" % VERSION
+    doc = ("\"\"\"\n" +
+           "This is a VERSION file and should NOT be manually altered"
+           + "\n\"\"\"")
+    doc += "\nversion = \"%s\"" % VERSION
 
-        if not filename:
-            filename = os.path.join(
-                    os.path.dirname(__file__), "Distributions.py", "version.py")
+    if not filename:
+        filename = os.path.join(
+                os.path.dirname(__file__), "Distributions.py", "version.py")
 
-        f = open(filename, "w")
-        try:
-            f.write(doc)
-        finally:
-            f.close()
+    f = open(filename, "w")
+    try:
+        f.write(doc)
+    finally:
+        f.close()
 
 write_version_py()
 
